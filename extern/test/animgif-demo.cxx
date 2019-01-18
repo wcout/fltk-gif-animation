0d69d255af35abd885be9aebe841e7cf9ddae4dd
wcout <wcout@users.noreply.github.com>
2019-01-18 16:43:43 +0100

    +extend demo

--- extern/test/animgif-demo.cxx
+++ extern/test/animgif-demo.cxx
@@ -10,22 +10,11 @@
 #include <FL/Fl_File_Chooser.H>
 #include <cstdio>
 
-// Note: Needed only to access the
-// protected Fl_Anim_GIF::draw() method :(
-class Anim_GIF : public Fl_Anim_GIF {
+class Fl_Anim_Button : public Fl_Button {
 public:
-  Anim_GIF(int x_, int y_, int w_, int h_, const char *name_) :
-    Fl_Anim_GIF(x_, y_, w_, h_, name_) {}
-    void draw() {
-      Fl_Anim_GIF::draw();
-    }
-};
-
-class AnimButton : public Fl_Button {
-public:
-  AnimButton(int x_, int y_, int w_, int h_, const char *gif_, const char *l_ = 0 ) :
+  Fl_Anim_Button(int x_, int y_, int w_, int h_, const char *gif_, const char *l_ = 0 ) :
     Fl_Button(x_, y_, w_, h_, l_) {
-    _anim = new Anim_GIF(x_, y_, 0, 0, gif_);
+    _anim = new Fl_Anim_GIF(x_, y_, 0, 0, gif_);
     fit();
   }
   void fit() {
@@ -44,17 +33,25 @@ public:
   int orig_w() const { return _orig_w; }
   int orig_h() const { return _orig_h; }
   Fl_Anim_GIF *anim() const { return _anim; }
-  ~AnimButton() {
+  void anim(Fl_Anim_GIF *anim_) {
+    if (anim_ == _anim) return;
+    parent()->remove(*_anim);
+    delete _anim;
+    _anim = anim_;
+    parent()->insert(*_anim, 0);
+    fit();
+  }
+  ~Fl_Anim_Button() {
     delete _anim;
   }
 private:
-  Anim_GIF *_anim;
+  Fl_Anim_GIF *_anim;
   int _orig_w;
   int _orig_h;
 };
 
 static void cb_start_stop(Fl_Widget *w_, void *d_) {
-  AnimButton *b = (AnimButton *)w_;
+  Fl_Anim_Button *b = (Fl_Anim_Button *)w_;
   if (b->anim()->playing()) {
     b->anim()->stop();
     b->anim()->frame(0);
@@ -67,7 +64,7 @@ static void cb_start_stop(Fl_Widget *w_, void *d_) {
 }
 
 static void cb_info(Fl_Widget *w_, void *d_) {
-  AnimButton *b = (AnimButton *)w_;
+  Fl_Anim_Button *b = (Fl_Anim_Button *)w_;
   if (Fl::event_button() > 1) {
     const char *filename = fl_file_chooser("Select a GIF image file","*.{gif,GIF}", NULL);
     if (!filename) return;
@@ -77,10 +74,16 @@ static void cb_info(Fl_Widget *w_, void *d_) {
     b->anim()->start();
     return;
   }
+  // just show off some features..
+  Fl_Anim_GIF *orig = b->anim()->copy(); // make a 1:1 backup copy of animation
+  orig->stop(); // stop backup animation
+  b->anim()->desaturate(); // show original desaturated
   fl_message("%d x %d (original: %d x %d)\n%d frames",
     b->anim()->canvas_w(), b->anim()->canvas_h(),
     b->orig_w(), b->orig_h(),
     b->anim()->frames());
+  b->anim(orig); // replace with backup
+  b->anim()->start(); // start again
 }
 
 int main(int argc_, char *argv_[]) {
@@ -88,11 +91,11 @@ int main(int argc_, char *argv_[]) {
 
   Fl_Double_Window win(300, 300, "animation demo");
 
-  AnimButton but1( 20, 10, 260, 50, "testsuite/filecopy.gif");
+  Fl_Anim_Button but1( 20, 10, 260, 50, "testsuite/filecopy.gif");
   but1.callback(cb_start_stop);
   but1.do_callback();
 
-  AnimButton but2( 100, 100, 100, 100, "testsuite/banana.gif" );
+  Fl_Anim_Button but2( 100, 100, 100, 100, "testsuite/banana.gif" );
   but2.align(FL_ALIGN_BOTTOM);
   but2.copy_label(but2.anim()->name());
   but2.callback(cb_info);
